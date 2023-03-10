1ea706efd54f85fa156c184f87a66f051f28e285
SakuraKat <SakuraKat1F338@gmail.com>
2023-03-10 20:25:07 +0530

    resolved merge conflict

--- config.py
+++ config.py
@@ -7,22 +7,31 @@ tickers = [
     'TSLA',
 ]
 
-up_msg = '${ticker} is up by ${unit}${change} (~${unit}${price}) BUY BUY BUY'
-down_msg = '${ticker} is down by ${unit}${change} (~${unit}${price}) SELL SELL SELL'
-same_msg = '${ticker} is the same (~${unit}${price}) HOLD HOLD HOLD'
+# Messages to send when a stock is up, down, or the same
+# ${ticker} is the ticker symbol
+# ${unit} is the currency unit
+# ${change} is the change in price
+# Default:
+# up_msg = '${ticker} is 📈up📈 by 📈${unit}${change} (~${unit}${price}) BUY BUY BUY'
+# down_msg = '${ticker} is 📉down📉 by 📉${unit}${change} (~${unit}${price}) SELL SELL SELL'
+# same_msg = '${ticker} is the same (~${unit}${price}) 🛑HOLD🛑 🛑HOLD🛑 🛑HOLD🛑'
+
+up_msg = '${ticker} is 📈up📈 by 📈${unit}${change} (~${unit}${price}) BUY BUY BUY'
+down_msg = '${ticker} is 📉down📉 by 📉${unit}${change} (~${unit}${price}) SELL SELL SELL'
+same_msg = '${ticker} is the same (~${unit}${price}) 🛑HOLD🛑 🛑HOLD🛑 🛑HOLD🛑'
 
 # If True, only send updates when a stock changes
 # If False, send updates every time the script runs
 # Default: True
-updates_only_mode = False
+updates_only_mode = True
 
 # Time between messages in seconds
 # Default: 10
-time_between_messages = 3
+time_between_messages = 10
 
 # Time between updates in seconds
 # Default: 60
-time_between_updates = 10
+time_between_updates = 60
 
 # Precision of the stock price
 # 0 is show the full price
